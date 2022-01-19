988d8311bc5348dd919133aeb4067762ba2db8ee
Vergil-SLI <sli23@choate.edu>
2022-01-19 14:52:38 -0600

    added distance calculation to the hub

--- subsystems/limelight.py
+++ subsystems/limelight.py
@@ -1,8 +1,24 @@
+from math import degrees, pi
 from networktables import NetworkTables
+import math
 
+class Limelight():
+    
+    table = NetworkTables.getTable("limelight")
+    tx = table.getNumber('tx',None)
+    ty = table.getNumber('ty',None)
+    ta = table.getNumber('ta',None)
+    ts = table.getNumber('ts',None)
+    
+    
+    def calculate_distance(self):
+        # values for calculation
+        cam_height = 1 # units inches
+        cam_angle = pi/4 # units radians
+        h_hub_height = 104 # units inches
+        h_hub_angle = math.radians(self.table.getNumber('ty',None))
+        
+        distance =  (h_hub_height - cam_height) / math.tan(cam_angle + h_hub_angle)
+        return distance
+    
 
-table = NetworkTables.getTable("limelight")
-tx = table.getNumber('tx',None)
-ty = table.getNumber('ty',None)
-ta = table.getNumber('ta',None)
-ts = table.getNumber('ts',None)

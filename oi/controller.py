95584c2a8b849b24629379144739302e309885b8
Sidharth Rao <sidharthmrao@gmail.com>
2022-01-25 17:34:33 -0500

    Completely working Incremental code.

--- oi/controller.py
+++ oi/controller.py
@@ -5,13 +5,13 @@ class controller():
     def __init__(self):
         # Define Joysticks
         self.driver_controller = wpilib.Joystick(0)
-    def get_y(self):
+    def get_y(self): ...
         # Get y-axis Value
-        return -self.driver_controller.getRawAxis(1) # Multiply by negative 1 so "UP" is Positive
-    def get_turn(self):
+        #return -self.driver_controller.getRawAxis(1) # Multiply by negative 1 so "UP" is Positive
+    def get_turn(self): ...
         # Get right joystick x-axis value (Speed)
-        return self.driver_controller.getRawAxis(4)
+        #return self.driver_controller.getRawAxis(4)
     def get_left_trigger(self):
-        return self.driver_controller.getRawAxis(2)
+        return self.driver_controller.getRawAxis(1)
     def get_right_trigger(self):
         return self.driver_controller.getRawAxis(3)
--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -11,38 +11,37 @@ class Shooter(SubsystemBase):
         self.motor_R = ctre.TalonFX(8) # TODO
 
         self.current_speed = 0
-        
-        self.change_coefficient = .005
+        self.max_speed = 1
 
-    #def roller_info(self): #Can set Speed_V if necessary
-    #    trajectoryVelocity = self.motor_L.getActiveTrajectoryVelocity()
-    #    outputPercent = self.motor_L.getMotorOutputPercent()
-    #    outputVoltage = self.motor_L.getMotorOutputVoltage()
-    #    return trajectoryVelocity, outputPercent, outputVoltage
+        self.increment = .0005
+
     def limit(self, value):
-        if value>1:
-            return .5
+        if value>self.max_speed:
+            return self.max_speed
         elif value<0:
             return 0
         else:
             return value
+
     def set_speed(self, speed:float):
         self.motor_L.set(ctre.ControlMode.PercentOutput, -speed)
-        #self.motor_R.set(ctre.ControlMode.PercentOutput, speed)
+        self.motor_R.set(ctre.ControlMode.PercentOutput, speed)
         self.current_speed = speed
-        #print(self.motor_L.getActiveTrajectoryVelocity())
-        #print(self.motor_R.getActiveTrajectoryVelocity())
-        print("Speed:", self.current_speed)
+        print("Current Speed:", self.current_speed)
+
     def alter_speed(self, speed:float):
         speed = self.limit(speed+self.current_speed)
+        print("Altering:", speed)
         self.set_speed(speed)
+
     def stop(self):
         self.set_speed(0)
+
     def controller_based(self, left_trigger:float, right_trigger:float):
-        if left_trigger:
-            self.alter_speed(-.0005)
-        if right_trigger:
-            self.alter_speed(.0005)
+        if left_trigger>.1 or left_trigger<-.1:
+            self.alter_speed(-self.increment)
+        if right_trigger>.1 or left_trigger<-.1:
+            self.alter_speed(self.increment)
 
         
 

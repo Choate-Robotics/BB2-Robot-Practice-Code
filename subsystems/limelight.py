5159de602e8bbd308ac9092d1d9386c3f293e1ff
Matthew Bardoe <mbardoe@gmail.com>
2022-01-31 17:38:13 -0500

    Update limelight.py with more comments
    Added information about what the variables from Network tables are available from limelight.

--- subsystems/limelight.py
+++ subsystems/limelight.py
@@ -4,19 +4,28 @@ import math
 
 class Limelight():
     
-    table = NetworkTables.getTable("limelight")
-    tx = table.getNumber('tx',None)
-    ty = table.getNumber('ty',None)
-    ta = table.getNumber('ta',None)
-    ts = table.getNumber('ts',None)
     
+    '''
+    tx = table.getNumber('tx',None)  # horizontal angle away from center (-27 to 27 degrees)
+    ty = table.getNumber('ty',None)  # vertical angle away from the center (-20.5 to 20.5 degrees)
+    ta = table.getNumber('ta',None)  # Area in percentage of image of the reflective surface that the limelight detects (pixels?)
+    ts = table.getNumber('ts',None)  # skew or rotation of the reflective surface from (-90 to 0 degrees)
+    '''
     
+    def __init__(self):
+        NetworkTables.initialize()
+        self.table = NetworkTables.getTable("limelight")
+        
     def calculate_distance(self):
         # values for calculation
-        cam_height = 1 # units inches
-        cam_angle = pi/4 # units radians
-        h_hub_height = 104 # units inches
-        h_hub_angle = math.radians(self.table.getNumber('ty',None))
+        cam_height = 1  # units inches (height from ground to camera)
+        cam_angle = pi/4  # units radians (angle from horizontal)
+        h_hub_height = 104  # units inches (where is the upper hub from ground) 8 feet 8 inches
+        ty=self.table.getNumber('ty',None)
+        if ty is None:
+            return -1
+        
+        h_hub_angle = math.radians(self.table.getNumber('ty',None))  # convert to radians
         
         distance =  (h_hub_height - cam_height) / math.tan(cam_angle + h_hub_angle)
         return distance
